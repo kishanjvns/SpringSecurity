55e8c44e82acdc6ae26c80edf4a434cb894e49f5
kishanj1 <kishan.jaiswal@zyter.com>
2024-11-21 16:32:40 +0530

    deploy app to external server

--- src/main/java/com/tech/kj/web/controller/MedicationController.java
+++ src/main/java/com/tech/kj/web/controller/MedicationController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/medication/")
+
 public class MedicationController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Hello World");
     }
